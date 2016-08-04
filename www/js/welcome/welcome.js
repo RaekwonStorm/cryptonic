cc75d70556a868fa8d678a0b552899dcc8c8ea3d
Reed Branson <reed.branson@gmail.com>
2016-08-04 15:59:32 -0400

    switching drivers

---
+++ www/js/welcome/welcome.js
@@ -0,0 +1,14 @@
+// cryptonic.config(function ($stateProvider) {
+
+//   $stateProvider.state('welcome', {
+//     url:,
+//     templateUrl:,
+
+//   })
+
+// })
+
+// cryptonic.controller('WelcomeCtrl', function ($scope, $ionicModal) {
+
+
+// })
