7727f1bfc5a257c84266092a80193768eb18941c
NICOLAAS KOSTER <nicolaas.koster@gmail.com>
2016-08-06 18:37:51 -0400

    boom shake the room

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,27 +2,35 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var cheerio = require('cheerio');
+var _ = require('lodash/core')
 
-// router.get('/scraper', function(req, res) {
-//     var url = 'https://www.coingecko.com/en/price_charts/ethereum/usd';
+router.get('/scraper', function(req, res) {
+    console.log('hello');
+    var url = 'https://www.coingecko.com/en/price_charts/ethereum/usd';
+    var ETH_ARRAY = [];
 
-//     request(url, function(error, response, html) {
-//         if (!error) {
+    request(url, function(error, response, html) {
+        if (!error) {
 
-//             var $ = cheerio.load(html);
+            var $ = cheerio.load(html, {
+                ignoreWhitespace: true
+            });
 
-//             var date, price;
-//             var json = { date: "", price: "" };
+            var date, price;
+            var json = { date: "", price: "" };
 
+            $('h2').each(function(i, element) {
+                // console.log('what dis', i, $(this).next().children().first().attr('data-prices'))
+                if (i === 3) ETH_ARRAY.push($(this).next().children().first().attr('data-prices'))
+            })
 
+            res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
+            res.send(JSON.parse(ETH_ARRAY[0]))
+        }
+    })
 
-//             $('h2').filter(function(){
-//             	return $(this).text() === "Ethereum (ETH/USD) 30 Days Price Chart";
-//             })
-
-//         }
-//     })
-// });
+});
 
 
 module.exports = router;
